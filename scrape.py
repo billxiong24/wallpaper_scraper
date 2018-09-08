76422bccea92fd3c806f3548b24295804e09a7ce
billxiong24 <billx0477@gmail.com>
2018-09-08 02:14:22 -0400

    comments, refactor

--- scrape.py
+++ scrape.py
@@ -1,7 +1,12 @@
+# Script to download wallpapers from Interfacelift.com
+# There are currently 395 pages of wallpapers. 
+# Downloads wallpapers in 1920x1080 resolution.
+
+
+
 from bs4 import BeautifulSoup
 import requests 
 from tqdm import tqdm
-import sys
 import argparse
 
 def check_args(start, end):
@@ -18,14 +23,13 @@ def check_args(start, end):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--start", type=int, default=0, help="Page to start at.")
-    parser.add_argument("-e", "--end", type=int, default=394, help="Page to end at.")
+    parser.add_argument("-s", "--start", type=int, default=1, help="Page to start at.")
+    parser.add_argument("-e", "--end", type=int, default=395, help="Page to end at.")
     args = parser.parse_args()
 
     if not check_args(args.start, args.end):
         print "Bad arguments. Exiting..."
         return 1
-
 
     print "STARTING PAGE: " + str(args.start)
     print "ENDING PAGE: " + str(args.end)
@@ -35,26 +39,32 @@ def main():
     for count in range(args.start, args.end + 1):
 
         print "------------------------------------------------------------PAGE " + str(count) + "------------------------------------------------------------"
+        # fetch html with wallpaper links
         response = requests.get(BASE_URL + "index" + str(count) + ".html") 
         soup = BeautifulSoup(response.text, 'html.parser')
         previews = soup.findAll("div", {"class": "preview"})
 
         for prev in previews:
+            # search for wallpaper links
             tag = prev.find("img")
             url = tag['src']
             name=tag['alt']
+            # Don't want spaces in file names
             name = name.replace(" ", "_") + ".jpg"
             name = name.replace("High-resolution_desktop_wallpaper_", "")
+            # Some file names include // or /, which is problematic
             name = name.replace("//","")
             name = name.replace("/","")
             # make higher resolution
             url = url.replace("672x420", "1920x1080")
+            # streaming request to download image
             res = requests.get(url, stream=True)
+            # write image to file
             with open(name, "wb") as handle:
                 for data in tqdm(res.iter_content(chunk_size=1024), desc="Downloading: "+url):
                     handle.write(data)
 
-    return 0        
+    return 0
 
 
 main()
